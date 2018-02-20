51ac1d013663f1101df2b2be14cbd4ecf7f54864
ZLHLHLH_coder <2623165118@qq.com>
2018-02-20 23:34:49 +0800

    new mei-tuan-index

---
+++ mei_tuan/js/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zlh on 2018.02.20.
+ */
+$(".right > ul > li").mouseover(function () {
+    $(this).children("ul").css("display","block");
+    $(this).css("border","1px solid #e5e5e5");
+})
+$(".right > ul > li").mouseout(function () {
+    $(this).children("ul").css("display","none");
+    $(this).css("border","0 solid #e5e5e5");
+})
